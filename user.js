3e2168b4f32a6780655c82b35b32835c2c3f0fb5
Maxwell23461 <40596264+Maxwell23461@users.noreply.github.com>
2019-02-19 03:29:55 +0000

    Update user.js

--- user.js
+++ user.js
@@ -5,7 +5,7 @@
 // @include      https://cytu.be/r/dostthoujeer
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @downloadURL  https://raw.githubusercontent.com/Maxwell23461/bigirononhiship/master/user.js
-// @updateURL    https://raw.githubusercontent.com/Maxwell23461/bigirononhiship/master/meta.js
+// @updateURL    https://raw.githubusercontent.com/Maxwell23461/bigirononhiship/master/user.js
 // @grant        none
 // ==/UserScript==
 

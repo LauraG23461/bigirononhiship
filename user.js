20e10b4aec94b4f6ca64d0a3432c8f3af575e61a
piwodog <36657129+piwodog@users.noreply.github.com>
2019-02-18 21:39:42 -0600

    Update user.js

--- user.js
+++ user.js
@@ -1,14 +1,3 @@
-// ==UserScript==
-// @name         Big Iron
-// @version      0.12
-// @author       The Boys
-// @include      https://cytu.be/r/dostthoujeer
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
-// @downloadURL  https://raw.githubusercontent.com/Maxwell23461/bigirononhiship/master/user.js
-// @updateURL    https://raw.githubusercontent.com/Maxwell23461/bigirononhiship/master/meta.js
-// @grant        none
-// ==/UserScript==
-
 var timer = setInterval (function() { scriptAutoRefresh(); }, 250);
 
 function addGlobalStyle(css) {
@@ -29,7 +18,7 @@ addGlobalStyle ( `
 function scriptAutoRefresh()
 {
     var replaceArry = [
-    [/genie/gi, 'ASS EATER'],
+    [/genie/gi, 'ASS EATER!!!!!!!'],
 
 ];
 var numTerms = replaceArry.length;

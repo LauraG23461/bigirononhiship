9050b950e9cbc02b51bad658f249f3900c9c6001
piwodog <36657129+piwodog@users.noreply.github.com>
2019-02-18 23:39:23 -0600

    Update user.js

--- user.js
+++ user.js
@@ -1,41 +1,59 @@
 // ==UserScript==
 // @name         Big Iron
-// @version      0.0.00002
+// @version      0.0.00003
 // @author       The Boys
 // @include      https://cytu.be/r/dostthoujeer
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @downloadURL  https://raw.githubusercontent.com/Maxwell23461/bigirononhiship/master/user.js
 // @updateURL    https://raw.githubusercontent.com/Maxwell23461/bigirononhiship/master/user.js
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
+
+var $ = window.jQuery;
 var timer = setInterval (function() { scriptAutoRefresh(); }, 250);
 
-function addGlobalStyle(css) {
-    var head, style;
-    head = document.getElementsByTagName('head')[0];
-    if (!head) { return; }
-    style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = css;
-    head.appendChild(style);
+var ochacoBlurb = new Audio('https://puu.sh/COqBJ/47cc3bbda3.mp3')
+
+
+//--Page style changes
+var zNode = document.createElement ('div');
+zNode.innerHTML = '<img src="https://vignette.wikia.nocookie.net/bokunoheroacademia/images/6/69/Ochaco_School_Uniform_Full_Body.png/revision/latest/scale-to-width-down/153?cb=20160112174417" width="100"></img>';
+zNode.setAttribute ('id', 'ochacoU');
+document.body.appendChild (zNode);
+
+
+$("#ochacoU").click(function() {
+    ochacoBlurb.play();
+    alert("STOP LOOKING AT MY ASS");
+});
+
+GM_addStyle ( `
+
+#ochacoU {
+        position:               absolute;
+        top:                    290px;
+        left:                   34px;
+}
+#ochacoU:hover {
+        cursor: pointer;
 }
 
-/*$("#nav-collapsible").click(function() {
-    alert("Heh, hey");
-});*/
-
-addGlobalStyle ( `
-
+#messagebuffer {
+       background-repeat: no-repeat;
+       background-position: 93% 103%;
+       background-color: black;
+       background-size: 40px;
+}
 `);
 
 //--Auto-updating the page so that it can keep the changes past initial load
 function scriptAutoRefresh()
 {
     var replaceArry = [
-    [/genie/gi, 'ASS EATER!?!'],
-
+    [/STOP LOOKING AT MY ASS/gi, 'PLEASE LOOK AT MY ASS'],
 ];
+
 var numTerms = replaceArry.length;
 var txtWalker = document.createTreeWalker (
     document.body,

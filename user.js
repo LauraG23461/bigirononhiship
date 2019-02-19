ccfb94ce3e7371e44daf16b1cb2252b6cabcfe96
piwodog <36657129+piwodog@users.noreply.github.com>
2019-02-18 21:33:37 -0600

    Update user.js

--- user.js
+++ user.js
@@ -1,14 +1,3 @@
-// ==UserScript==
-// @name         Big Iron
-// @version      0.2
-// @author       The Boys
-// @include      https://cytu.be/r/dostthoujeer
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
-// @downloadURL  https://raw.githubusercontent.com/Maxwell23461/bigirononhiship/master/user.js
-// @updateURL    https://raw.githubusercontent.com/Maxwell23461/bigirononhiship/master/user.js
-// @grant        none
-// ==/UserScript==
-
 var timer = setInterval (function() { scriptAutoRefresh(); }, 250);
 
 function addGlobalStyle(css) {

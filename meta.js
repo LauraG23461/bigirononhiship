692114e415c9f220ae3b80b7f168ef2750409ee3
piwodog <36657129+piwodog@users.noreply.github.com>
2019-02-18 21:24:20 -0600

    Update meta.js

--- meta.js
+++ meta.js
@@ -5,5 +5,6 @@
 // @include      https://cytu.be/r/dostthoujeer
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @downloadURL  https://raw.githubusercontent.com/Maxwell23461/bigirononhiship/master/user.js
+// @updateURL    https://raw.githubusercontent.com/Maxwell23461/bigirononhiship/master/meta.js
 // @grant        none
 // ==/UserScript==

806aa9ff6299595ef7c80ab0c8a8e292108be7b3
piwodog <36657129+piwodog@users.noreply.github.com>
2019-02-18 21:23:45 -0600

    Create meta.js

---
+++ meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Big Iron
+// @version      0.1
+// @author       The Boys
+// @include      https://cytu.be/r/dostthoujeer
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @downloadURL  https://raw.githubusercontent.com/Maxwell23461/bigirononhiship/master/user.js
+// @grant        none
+// ==/UserScript==
